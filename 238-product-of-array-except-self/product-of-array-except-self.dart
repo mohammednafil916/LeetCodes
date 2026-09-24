95ce96af9343b1275f8e5e8a16cbbad0a30d7aa4
Mohammed Nafil <nafilpulikkalakath916@gmail.com>
2026-09-24 16:57:27 +0530

    Time: 25 ms (5.45%) | Memory: 166.4 MB (47.27%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.dart
@@ -0,0 +1,21 @@
+class Solution {
+  List<int> productExceptSelf(List<int> nums) {
+    List<int> answer = List.filled(nums.length, 1);
+
+    int left = 1;
+
+    for (int i = 0; i < nums.length; i++) {
+      answer[i] = left;
+      left *= nums[i];
+    }
+
+    int right = 1;
+
+    for (int i = nums.length - 1; i >= 0; i--) {
+      answer[i] *= right;
+      right *= nums[i];
+    }
+
+    return answer;
+  }
+}
